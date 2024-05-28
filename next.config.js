adce561d763fb57feb09815df53112528c023056
AI BIGDATA <samirpoudel@Samirs-MacBook-Air.local>
2024-05-28 17:59:33 -0400

    Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    trailingSlash: true,
+    // If your site is served from a subpath, uncomment and replace 'your-repo-name'
+    // assetPrefix: '/your-repo-name/',
+    // basePath: '/your-repo-name',
+  };
+  module.exports = {
+    output: 'export',
+  };
+  
