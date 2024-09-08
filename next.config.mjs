eb7f81f8034e1b389b94605caf2518245dc7986d
theoriginalsam <spoudel04@gmail.com>
2024-09-08 18:29:51 -0500

    Deploy static site to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   // Enabling static export
   output: 'export',
   // Optionally, add basePath if you're deploying under a subpath
-  basePath: '/',
-  assetPrefix: '/',
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
