a0af1055ed7585eea599455853860080aea22a22
theoriginalsam <spoudel04@gmail.com>
2024-09-08 18:13:12 -0500

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enabling static export
+  output: 'export',
+  // Optionally, add basePath if you're deploying under a subpath
+  basePath: '',
+  assetPrefix: '',
 };
 
 export default nextConfig;
